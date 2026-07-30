11eda0e524ce3c6162c0f2a731cc82eb69f309f5
Gloria Martinez <gloriacitizen.00@gmail.com>
2026-07-30 11:00:57 -0600

    Remove heading update loop from script.js
    Removed the setInterval function that updated the heading.

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
 const degrees = document.getElementById("degrees");
 
-
 let heading = 0;
 
 
-setInterval(()=>{
-
-
-    heading++;
-
-
-    if(heading >= 360){
-
-        heading = 0;
-
-    }
-
-
-    degrees.textContent =
-        heading.toString().padStart(3,"0") + "°";
-
-
-},100);
+// modo prueba detenido
+degrees.textContent = 
+    heading.toString().padStart(3,"0") + "°";
